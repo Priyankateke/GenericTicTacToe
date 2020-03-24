c252d590b37be419b93232ad4b01bd4439a71592
Priyankateke <priiyankateke1705@gmail.com>
2020-03-24 06:27:34 -0700

    ADDED: Resetting Game Board

--- genericTicTacToe.sh
+++ genericTicTacToe.sh
@@ -1,3 +1,21 @@
 #!/bin/bash -x
 
 echo "Welcome To Generic Tic Tac Toe"
+
+declare -a gameBoard
+
+read -p "Enter Board Size : " BOARD_SIZE
+
+#Restting game board
+function resetBoard()
+{
+	for((row=0;row<BOARD_SIZE;row++))
+	do
+		for((column=0;column<BOARD_SIZE;column++))
+		do
+			gameBoard[$row,$column]='-'
+		done
+	done
+}
+
+resetBoard
